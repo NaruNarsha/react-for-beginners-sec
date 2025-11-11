bc25da1795ea6c7fc4032721ca130c4029e50f8e
문경림 <naru@takeone.co.kr>
2025-11-11 20:20:03 +0900

    udpate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+//import Button from "./Button"
+//import styles from "./App.module.css"
+import { useState, useEffect } from "react";
 
 
 function App() {
-  return (
-   <div>
-    <h1>Welcome back!</h1>
-   </div>
-  );
+
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+
+  console.log('i run all the time');
+
+  useEffect(() => {
+    console.log("Call the API....")
+  }, []);
+
+  return(
+    <div>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
+    </div>
+  )
 }
 
 export default App;
